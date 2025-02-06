a4fd245ad298ea7795d983ab57576b396e9861aa
Paulas Ndalahwa George <wxjhlw@inf.elte.hu>
2025-02-06 09:06:20 +0100

    change powemetrics benchmark logging to not using PID when stopping

--- src/main/erlang/benchmarks/benchmark_runner.erl
+++ src/main/erlang/benchmarks/benchmark_runner.erl
@@ -1,7 +1,7 @@
 -module(benchmark_runner).
 -export([run/2]).
 % Power metrics functions
--export([start_power_metrics/0, log_idle_power/0, trigger_power_sample/1, flush_power_data/1, stop_power_metrics/0, generate_log_filename/1]).
+-export([start_power_metrics/0, log_idle_power/0,stop_power_metrics/0, generate_log_filename/1]).
 
 run(BenchmarkModule, NumIterations) ->
         % Log idle power consumption before benchmarking
@@ -40,24 +40,22 @@ log_idle_power() ->
     PowerMetricsCmd = "sudo powermetrics -s cpu_power -n 5 -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | grep -i \"Intel energy model derived CPU core power\" | while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> " ++ IdleLogFile ++ "; done",
     % PowerMetricsCmd = "sudo powermetrics -s cpu_power -n 5 -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info > " ++ IdleLogFile ++ " 2>&1", 
     os:cmd(PowerMetricsCmd),
-    timer:sleep(5000),  % Give time to capture idle power samples
+    timer:sleep(6000),  % Give time to capture idle power samples
     IdleLogFile.
+
+
+% start_power_metrics() ->
+%     BenchmarkLogFile = generate_log_filename("power_metrics"),
+%     % PowerMetricsCmd = "sudo powermetrics -s cpu_power -n 1 -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info > " 
+%     %                   ++ BenchmarkLogFile ++ " 2>&1 & echo $!",
+%     % PowerMetricsCmd = "sudo powermetrics -s cpu_power -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | grep -i \"Intel energy model derived CPU core power\" | while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> "
+%     %                     ++ BenchmarkLogFile ++ "; done 2>&1 & echo $!",
+%     PowerMetricsCmd = "sudo powermetrics -s cpu_power  -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | grep -i \"Intel energy model derived CPU core power\" | while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> " ++ BenchmarkLogFile ++ "; done",
+%     os:cmd(PowerMetricsCmd),
+%     BenchmarkLogFile.
     
-
-
-start_power_metrics() ->
-    % PowerMetricsCmd = "sudo powermetrics -s cpu_power -n 1 -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info > " 
-    %                   ++ LogFile ++ " 2>&1 & echo $!",
-    BenchmarkLogFile = generate_log_filename("power_metrics"),
-    % PowerMetricsCmd = "sudo powermetrics -s cpu_power  -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | grep -i \"Intel energy model derived CPU core power\" | while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> " ++ BenchmarkLogFile ++ "; done",
-
-    % PowerMetricsCmd = "sudo powermetrics -s cpu_power -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info > " 
-    %                   ++ BenchmarkLogFile ++ " 2>&1 & echo $!",
-    PowerMetricsCmd = "sudo powermetrics -s cpu_power -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | grep -i \"Intel energy model derived CPU core power\" | while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> " ++ BenchmarkLogFile ++ "; done",
-    os:cmd(PowerMetricsCmd),
-    timer:sleep(1000), 
-    io:format("PowerMetrics started Logging to file: ~s~n", [BenchmarkLogFile]),
-    BenchmarkLogFile.
+    
+    % PidStr = string:trim(os:cmd(PowerMetricsCmd)),  % Trim newline
     % case catch list_to_integer(PidStr) of
     %     Pid when is_integer(Pid) -> 
     %         io:format("PowerMetrics started with PID: ~p~nLogging to file: ~s~n", [Pid, BenchmarkLogFile]),
@@ -67,29 +65,33 @@ start_power_metrics() ->
     %         exit(start_power_metrics_failed)
     % end.
 
+start_power_metrics() ->
+    BenchmarkLogFile = generate_log_filename("power_metrics"),
+    PowerMetricsCmd = "sudo powermetrics -s cpu_power -i 1000 -a 0 --hide-cpu-duty-cycle --show-extra-power-info | "
+                        "grep -i \"Intel energy model derived CPU core power\" | "
+                        "while read line; do echo \"$(date '+%Y-%m-%d %H:%M:%S') $line\" >> " ++ BenchmarkLogFile ++ "; done &",
+    os:cmd(PowerMetricsCmd),
+    timer:sleep(2000),  % Allow time for powermetrics to start
+    BenchmarkLogFile.
+    
 
 
-% Trigger an immediate power sample
-trigger_power_sample(Pid) ->
-    os:cmd("sudo kill -29 " ++ integer_to_list(Pid)).
-    % os:cmd("sudo kill -SIGINFO " ++ integer_to_list(Pid)).
 
 
-% Flush power data
-flush_power_data(Pid) ->
-    os:cmd("sudo kill -23 " ++ integer_to_list(Pid)).
-    % os:cmd("sudo kill -SIGIO " ++ integer_to_list(Pid)).
+% stop_power_metrics(Pid) ->
+%     % os:cmd("sudo pkill -2 powermetrics"),
+%     os:cmd("sudo kill -SIGTERM " ++ integer_to_list(Pid)),
+%     {_Date, Time} = calendar:universal_time(),
+%     FormattedTimestamp = io_lib:format("~p:~p:~p", tuple_to_list(Time)),
+%     io:format("PowerMetrics stopped at ~p.~n", [lists:flatten(FormattedTimestamp)]).
 
-% Stop powermetrics
 stop_power_metrics() ->
-    % os:cmd("sudo kill -15 " ++ integer_to_list(Pid)),
-    % os:cmd("sudo kill -SIGTERM " ++ integer_to_list(Pid)),
-    % os:cmd("sudo kill -2 " ++ integer_to_list(Pid)),
-    % os:cmd("sudo kill -SIGINT " ++ integer_to_list(Pid)),
     os:cmd("sudo pkill -2 powermetrics"),
     {_Date, Time} = calendar:universal_time(),
     FormattedTimestamp = io_lib:format("~p:~p:~p", tuple_to_list(Time)),
-    io:format("PowerMetrics stopped at ~p.~n",[lists:flatten(FormattedTimestamp)]).
+    io:format("PowerMetrics stopped at ~p.~n", [lists:flatten(FormattedTimestamp)]).
+
+
 
 generate_log_filename(BaseName) ->
     BaseName ++ ".log".
