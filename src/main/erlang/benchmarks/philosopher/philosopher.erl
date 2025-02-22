d16ad0408d6c622ba678d805c0937ca3c71d7df4
Paulas Ndalahwa George <wxjhlw@inf.elte.hu>
2025-02-22 10:32:10 +0100

    complete erlang philosopher benchmark

--- src/main/erlang/benchmarks/philosopher/philosopher.erl
+++ src/main/erlang/benchmarks/philosopher/philosopher.erl
@@ -2,33 +2,41 @@
 -export([run/0, print_config/0]).
 
 -define(N, 20).      % Number of philosophers
--define(M, 10_000).   % Number of eating rounds
+-define(M, 10_000).  % Number of eating rounds
 
 run() ->
     Main = self(),
     Arbitrator = spawn(fun() -> arbitrator(Main, ?N, #{}, 0) end),
-    [spawn(fun() -> philosopher(Id, 0, ?M, Arbitrator) end) || Id <- lists:seq(0, ?N - 1)],
+    PhilosopherPids = [spawn(fun() -> philosopher(Id, 0, ?M, Arbitrator) end) || Id <- lists:seq(0, ?N - 1)],
+
+    % Send initial message to philosophers
+    [Pid ! start || Pid <- PhilosopherPids],
 
     receive
-      {done, TotalRetries} ->
-        io:format("Dining Philosophers benchmark completed.~n"),
-        io:format("Total Retries: ~p~n", [TotalRetries])
+        {done, TotalRetries} ->
+            % io:format("Dining Philosophers benchmark completed.~n"),
+            io:format("Total retries: ~p~n", [TotalRetries])
     end.
 
-    
-philosopher(_Id, Retries, 0, Arbitrator) ->
-    Arbitrator ! {exit, Retries};
-
-philosopher(Id, Retries, RoundsLeft, Arbitrator) ->
-    Arbitrator ! {hungry, self(), Id},
+philosopher(Id, Retries, 0, Arbitrator) ->
+    % io:format("Philosopher ~p has finished eating.~n", [Id]),
+    Arbitrator ! {exit, Id, Retries};
+philosopher(Id, Retries, Rounds, Arbitrator) ->
     receive
-        eat ->
-            Arbitrator ! {done, Id},
-            philosopher(Id, Retries, RoundsLeft - 1, Arbitrator);
+        start ->
+            % io:format("Philosopher ~p is hungry.~n", [Id]),
+            Arbitrator ! {hungry, self(), Id},
+            philosopher(Id, Retries, Rounds, Arbitrator);
         denied ->
-            philosopher(Id, Retries + 1, RoundsLeft, Arbitrator)
+            % io:format("Philosopher ~p was denied.~n", [Id]),
+            Arbitrator ! {hungry, self(), Id},
+            philosopher(Id, Retries + 1, Rounds, Arbitrator);
+        eat ->
+            % io:format("Philosopher ~p is eating.~n", [Id]),
+            Arbitrator ! {done, Id},
+            self() ! start,
+            philosopher(Id, Retries, Rounds - 1, Arbitrator)
     end.
-
 
 arbitrator(Main, 0, _Forks, TotalRetries) -> 
     Main ! {done, TotalRetries};
@@ -36,27 +44,29 @@ arbitrator(Main, 0, _Forks, TotalRetries) ->
 arbitrator(Main, N, Forks, TotalRetries) ->
     receive
         {hungry, Philosopher, Id} ->
-            Left = Id,
-            Right = (Id + 1) rem ?N,
-            case {maps:get(Left, Forks, false), maps:get(Right, Forks, false)} of
+            LeftFork = Id,
+            RightFork = (Id + 1) rem ?N,
+            case {maps:get(LeftFork, Forks, false), maps:get(RightFork, Forks, false)} of
                 {false, false} -> 
-                    NewForks = Forks#{Left => true, Right => true},
+                    % io:format("Philosopher ~p can eat.~n", [Id]),
+                    NewForks = Forks#{LeftFork => true, RightFork => true},
                     Philosopher ! eat,
                     arbitrator(Main, N, NewForks, TotalRetries);
                 _ ->  
+                    % io:format("Philosopher ~p is denied forks.~n", [Id]),
                     Philosopher ! denied,
                     arbitrator(Main, N, Forks, TotalRetries)
             end;
-
         {done, Id} ->
-            Left = Id,
-            Right = (Id + 1) rem ?N,
-            arbitrator(Main, N, Forks#{Left => false, Right => false}, TotalRetries);
-
-        {exit, LocalRetries} ->
+            LeftFork = Id,
+            RightFork = (Id + 1) rem ?N,
+            % io:format("Philosopher ~p has finished eating and released forks.~n", [Id]),
+            arbitrator(Main, N, Forks#{LeftFork => false, RightFork => false}, TotalRetries);
+        {exit, _Id, LocalRetries} ->
+            % io:format("Philosopher ~p exits with ~p retries.~n", [Id, LocalRetries]),
             arbitrator(Main, N - 1, Forks, TotalRetries + LocalRetries)
     end.
 
 print_config() ->
-    io:format("    N (num philosophers) = ~p~n",[?N]),
-    io:format("    M (num eating rounds) = ~p~n",[?M]).
+    io:format("    N (num philosophers) = ~p~n", [?N]),
+    io:format("    M (num eating rounds) = ~p~n", [?M]).
