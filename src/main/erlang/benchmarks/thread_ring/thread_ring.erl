a7fd3773aeb5ea7bc615375671de31505622c9cc
Paulas Ndalahwa George <wxjhlw@inf.elte.hu>
2025-03-04 13:13:03 +0100

    fix: use PID instead of registering the main process

--- src/main/erlang/benchmarks/thread_ring/thread_ring.erl
+++ src/main/erlang/benchmarks/thread_ring/thread_ring.erl
@@ -1,19 +1,17 @@
 -module(thread_ring).
--export([start/2, loop/2,run/0, print_config/0]).
--define(N, 100000). % number of actors
+-export([run/0, actor/3, print_config/0]).
+-define(N, 100). % number of actors
 -define(R, 100_000). % number of pings/no of rounds
 
-run() ->
-    register(main, self()),
-    start(?N, ?R),
+run() -> 
+    Main = self(),
+    Pids = [spawn(?MODULE, actor, [self(), undefined, Main]) || _ <- lists:seq(1, ?N)],
+    setup_ring(Pids),
+    hd(Pids) ! {ping, ?R},
     receive
       done ->
         ok
     end.
-start(N, R) -> 
-    Pids = [spawn(?MODULE, loop, [self(), undefined]) || _ <- lists:seq(1, N)],
-    setup_ring(Pids),
-    hd(Pids) ! {ping, R}.
 
 setup_ring([H | T]) ->
     setup_ring(H, T ++ [H]).
@@ -24,18 +22,18 @@ setup_ring(Current, [Next | Rest]) ->
     Current ! {set_next, Next},
     setup_ring(Next, Rest).
 
-loop(Self, Next) ->
+actor(Self, Next, Main) ->
     receive
         {set_next, NewNext} ->
-            loop(Self, NewNext);
+            actor(Self, NewNext, Main);
         {ping, PingsLeft} when PingsLeft > 0 ->
             Next ! {ping, PingsLeft - 1},
-            loop(Self, Next);
+            actor(Self, Next, Main);
         {ping, 0} ->
             Next ! {exit, Self},  %% Send exit message with the first actor's PID
-            loop(Self, Next);
+            actor(Self, Next, Main);
         {exit, Original} when Self =:= Original -> %% Terminate when exit message loops back
-            main ! done;
+            Main ! done;
         {exit, Original} ->
             Next ! {exit, Original},
             ok
